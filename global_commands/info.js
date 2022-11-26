87d108da8f12ba3875bed11f3e548be8b46c034f
david <89749147+analnyroztahovac@users.noreply.github.com>
2022-11-26 21:11:34 +0100

    odstranene constanty
    pri global commande info >> odstranene zbytocne constanty, upraveny format

--- global_commands/info.js
+++ global_commands/info.js
@@ -23,23 +23,33 @@ module.exports = {
     // Spustenie funkcie prikazu
     run: async ( { interaction } ) => {
         
-        const play_prikaz = `**+>** \`search\` argument sluzi na vyhladanie pesnicky podla jej nazvu \n**+>** \`song\` argument sluzi na vyhladnie pesnicky podla jej URL adresy \n**+>** \`playlist\` argument sluzi na vyhladanie playlistu podla jeho URL adresy`
-        const poradie_prikaz = 'Prikaz sluzi na zobrazenie aktualne hranej pesnicky ( *ak nejaka je* ) a za nou nasledujuce v poradi ( *ak nejake su* ) \n**+>** \`strana\` je optioal argument pre zobrazenie poradia na zadanej strane'
-        const skip_prikaz = 'Prikaz sluzi na preskocanie aktualnej pesnicky a zaroven automaticky pustenie za nou nasledujucu.'  
-        const stop_prikaz = 'Prikaz sluzi na ukoncenie prehravania aktualnej pesnicky a zaroven za nou nasledujuce ( *ak nejake su* )'
-
         // Embed const
         const Embed = new EmbedBuilder()
             .setTitle(`Hudba ${emoji_check}`)
             .setDescription('Open source, jednoduchy a napriek tomu efektivny discord music bot.\nSource kod mozno najst na: https://github.com/analnyroztahovac/Music-Bot\n**Zoznam prikazov:**')
             .setColor(farba_nonerror)
             .addFields(
-                { name: 'Prikaz \`/play search | song | playlist\`:', value: `${play_prikaz}` },
-                { name: 'Prikaz \`/poradie ( strana )\`:', value: `${poradie_prikaz}` },
-                { name: 'Prikaz \`/skip\`:', value: `${skip_prikaz}` },
-                { name: 'Prikaz \`/stop\`:', value: `${stop_prikaz}` }
+                { 
+                    name: 'Prikaz \`/play search | song | playlist\`:',
+                    value: `**+>** \`search\` argument sluzi na vyhladanie pesnicky podla jej nazvu\n
+                            **+>** \`song\` argument sluzi na vyhladnie pesnicky podla jej URL adresy\n
+                            **+>** \`playlist\` argument sluzi na vyhladanie playlistu podla jeho URL adresy` 
+                },
+                { 
+                    name: 'Prikaz \`/poradie ( strana )\`:',
+                    value: `Prikaz sluzi na zobrazenie aktualne hranej pesnicky ( *ak nejaka je* ) a za nou nasledujuce v poradi ( *ak nejake su* )\n
+                            **+>** \`strana\` je optioal argument pre zobrazenie poradia na zadanej strane` 
+                },
+                {
+                    name: 'Prikaz \`/skip\`:',
+                    value: `Prikaz sluzi na preskocanie aktualnej pesnicky a zaroven automaticky pustenie za nou nasledujucu.`
+                },
+                { 
+                    name: 'Prikaz \`/stop\`:',
+                    value: `Prikaz sluzi na ukoncenie prehravania aktualnej pesnicky a zaroven za nou nasledujuce ( *ak nejake su* )` 
+                }
                 )
-            .setFooter( { text: `${footer}`, iconURL: `${footer_icon}` } )
+            .setFooter( { text: footer, iconURL: footer_icon } )
         
         // Odpovieme na interaction
         return interaction.editReply( { embeds: [Embed], ephemeral: true } )
